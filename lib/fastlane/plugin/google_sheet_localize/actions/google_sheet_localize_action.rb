15035766f82c62c76c85d79c121e6c5f0eb6fd74
Mario Hahn <mario_hahn@me.com>
2019-02-05 11:01:18 +0100

    Improved Action

--- lib/fastlane/plugin/google_sheet_localize/actions/google_sheet_localize_action.rb
+++ lib/fastlane/plugin/google_sheet_localize/actions/google_sheet_localize_action.rb
@@ -6,45 +6,243 @@ module Fastlane
     class GoogleSheetLocalizeAction < Action
       def self.run(params)
 
-        UI.message("Using config file: #{params[:localize_credentials]}")
+        session = GoogleDrive::Session.from_service_account_key(params[:service_account_path])
+        spreadsheet_id = "https://docs.google.com/spreadsheets/d/#{params[:sheet_id]}"
+        tabs = params[:tabs]
+        plaform = params[:platform]
 
-        session = Helper::GoogleSheetLocalizeHelper.setup(params[:localize_credentials])
-        ##
-        # Ensure valid credentials, either by restoring from the saved credentials
-        # files or intitiating an OAuth2 authorization. If authorization is required,
-        # the user's default browser will be launched to approve the request.
-        #
-
-        # Prints the names and majors of students in a sample spreadsheet:
-        # https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-        spreadsheet_id = params[:localize_sheet_id]
-        spreadsheet_url = "https://docs.google.com/spreadsheets/d/#{spreadsheet_id}"
-
-        range = 'Class Data!A2:E'
-        spreadsheet = session.spreadsheet_by_url(spreadsheet_url)
+        spreadsheet = session.spreadsheet_by_url(spreadsheet_id)
 
         # Get the first worksheet
         worksheet = spreadsheet.worksheets.first
 
-        languages = Helper::GoogleSheetLocalizeHelper.numberOfLanguages(worksheet)
-        puts languages
-
-        contentRows = worksheet.rows.drop(1)
+        languages = numberOfLanguages(worksheet)
 
         result = []
+
         for i in 2..1+languages
           title = worksheet.rows[0][i]
 
           language = {
             'language' => title,
-            'items' => Helper::GoogleSheetLocalizeHelper.generateJSONObject(contentRows, i)
+            'items' => []
           }
+
+          filterdWorksheets = []
+
+          if tabs.count == 0
+            filterdWorksheets = spreadsheet.worksheets
+          else
+            filterdWorksheets = spreadsheet.worksheets.select { |item| tabs.include?(item.title) }
+          end
+
+          filterdWorksheets.each { |worksheet|
+            contentRows = worksheet.rows.drop(1)
+            language['items'].concat(generateJSONObject(contentRows, i))
+          }
+
           result.push(language)
+
+        end
+        createFiles(result, platform, path)
+      end
+
+      def generateJSONObject(contentRows, index)
+          result = Array.new
+          for i in 0..contentRows.count - 1
+              item = generateSingleObject(contentRows[i], index)
+
+              if item[:identifierIos] != "" && item[:identifierAndroid] != ""
+                result.push(item)
+              end
+          end
+
+          return result
+
+      end
+
+      def writeToJSONFile(languages)
+        File.open("output.json","w") do |f|
+          f.write(JSON.pretty_generate(languages))
+        end
+      end
+
+      def generateSingleObject(row, column)
+        identifierIos = row[0]
+        identifierAndroid = row[1]
+
+        text = row[column]
+        comment = row.last
+
+        object = { 'identifierIos' => identifierIos,
+                   'identifierAndroid' => identifierAndroid,
+                   'text' => text,
+                   'comment' => comment
+        }
+
+        return object
+
+      end
+
+      def createFiles(languages, platform, destinationPath)
+          languages.each { |language| createFileForLanguage(language, platform, destinationPath) }
+
+          if platform == "ios"
+
+            swiftFilename = "Localization.swift"
+            swiftFilepath = "#{destinationPath}/#{swiftFilename}"
+
+            #removes all identififiers with the label Not Required
+            filteredItems = languages[0]["items"].select { |item| item['identifierIos'] != "NR" }
+
+            File.open(swiftFilepath, "w") do |f|
+              f.write("import Foundation\n\n\npublic struct Localization {\n")
+              filteredItems.each { |item|
+
+                identifier = item['identifierIos']
+
+                values = identifier.dup.gsub('.', ' ').split(" ")
+
+                constantName = ""
+
+                values.each_with_index do |item, index|
+                  if index == 0
+                    constantName += item.downcase
+                  else
+                    constantName += item.capitalize
+                  end
+                end
+
+                text = mapInvalidPlaceholder(item['text'])
+
+                arguments = findArgumentsInText(text)
+
+                puts "Arguments"
+                puts arguments.count
+
+                if arguments.count == 0
+                  f.write("\n\t///Sheet comment: #{item['comment']}\n\tpublic static let #{constantName} = localized(identifier: \"#{identifier}\")\n")
+                else
+                  f.write(createiOSFunction(constantName, identifier, arguments, item['comment']))
+                end
+              }
+              f.write("\n}")
+              f.write(createiOSFileEndString())
+            end
+
+          end
+      end
+
+      def createFileForLanguage(language, platform, destinationPath)
+        if platform == "ios"
+
+          swiftFilename = "Localization.swift"
+          swiftFilepath = "#{destinationPath}/#{swiftFilename}"
+
+          #removes all identififiers with the label Not Required
+          filteredItems = language["items"].select { |item| item['identifierIos'] != "NR" }
+
+          filename = "Localizable.strings"
+          filepath = "#{destinationPath}/#{language['language']}.lproj/#{filename}"
+          FileUtils.mkdir_p language['language']
+          File.open(filepath, "w") do |f|
+            filteredItems.each { |item|
+
+              text = mapInvalidPlaceholder(item['text'])
+
+              f.write("//#{item['comment']}\n\"#{item['identifierIos']}\" = \"#{text}\";\n")
+            }
+          end
+        end
+
+        if platform == "android"
+          FileUtils.mkdir_p "values-#{language['language']}"
+          File.open("values-#{language['language']}/strings.xml", "w") do |f|
+            f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+            f.write("<resources>\n")
+            language["items"].each { |item|
+              f.write("\t<!--#{item['comment']}-->\n\t<string name=\"#{item['identifierAndroid']}\"><![CDATA[#{item['text']}]]></string>\n")
+            }
+            f.write("</resources>\n")
+          end
+        end
+      end
+
+      def createiOSFileEndString()
+        return "\n\nextension Localization {\n\tprivate static func localized(identifier key: String, _ args: CVarArg...) -> String {\n\t\tlet format = NSLocalizedString(key, tableName: nil, bundle: Bundle.main, comment: \"\")\n\n\t\tguard !args.isEmpty else { return format }\n\n\t\treturn String(format: format, locale: Locale.current, arguments: args)\n\t}\n}"
+      end
+
+      def createiOSFunction(constantName, identifier, arguments, comment)
+          functionTitle = "\n\t///Sheet comment: #{comment}\n\tpublic static func #{constantName}("
+
+          arguments.each_with_index do |item, index|
+            puts item
+            functionTitle = functionTitle + "_ arg#{index}: #{item[:type]}"
+            if index < arguments.count - 1
+              functionTitle = functionTitle + ", "
+            else
+              functionTitle = functionTitle + ") -> String {\n"
+            end
+          end
+          functionTitle = functionTitle + "\t\treturn localized(identifier: \"#{identifier}\", "
+          arguments.each_with_index do |item, index|
+            functionTitle = functionTitle + "arg#{index}"
+            if index < arguments.count - 1
+              functionTitle = functionTitle + ", "
+            else
+              functionTitle = functionTitle + ")\n\t}"
+            end
+          end
+
+          return functionTitle
+      end
+
+      def findArgumentsInText(text)
+        result = Array.new
+        filtered = mapInvalidPlaceholder(text)
+
+        puts filtered
+
+        stringIndexes = (0 ... filtered.length).find_all { |i| filtered[i,2] == '%@' }
+        intIndexes = (0 ... filtered.length).find_all { |i| filtered[i,2] == '%d' }
+        floatIndexes = (0 ... filtered.length).find_all { |i| filtered[i,2] == '%f' }
+        doubleIndexes = (0 ... filtered.length).find_all { |i| filtered[i,3] == '%ld' }
+
+        if stringIndexes.count > 0
+          result = result.concat(stringIndexes.map { |e| { "index": e, "type": "String" }})
+        end
+
+        if intIndexes.count > 0
+          result = result.concat(intIndexes.map { |e| { "index": e, "type": "Int" }})
+        end
+
+        if floatIndexes.count > 0
+          result = result.concat(floatIndexes.map { |e| { "index": e, "type": "Float" }})
+        end
+
+        if doubleIndexes.count > 0
+          result = result.concat(doubleIndexes.map { |e| { "index": e, "type": "Double" }})
         end
 
         puts result
-        Helper::GoogleSheetLocalizeHelper.writeToJSONFile(result)
-        Helper::GoogleSheetLocalizeHelper.createiOSFiles(result)
+        result = result.sort { |a, b| a[:index] <=> b[:index] }
+        puts result
+
+        puts "count"
+        puts result.count
+
+
+        return result
+      end
+
+      def mapInvalidPlaceholder(text)
+        filtered = text.gsub('%s', '%@')
+        return filtered
+      end
+
+      def numberOfLanguages(worksheet)
+        i = worksheet.num_cols
+        return i - 3
       end
 
       def self.description
@@ -66,16 +264,26 @@ module Fastlane
 
       def self.available_options
         [
-           FastlaneCore::ConfigItem.new(key: :localize_sheet_id,
-                                   env_name: "LOCALIZE_SHEET_ID",
+          FastlaneCore::ConfigItem.new(key: :service_account_path,
+                                  env_name: "SERVICE_ACCOUNT_PATH",
+                               description: "Credentials path",
+                                  optional: false,
+                                      type: String),
+           FastlaneCore::ConfigItem.new(key: :sheet_id,
+                                   env_name: "SHEET_ID",
                                 description: "Your Google-spreadsheet id",
                                    optional: false,
                                        type: String),
-           FastlaneCore::ConfigItem.new(key: :localize_credentials,
-                                   env_name: "LOCALIZE_CREDENTIALS",
-                                description: "Credentials Key path",
-                                   optional: false,
-                                       type: String)
+          FastlaneCore::ConfigItem.new(key: :platform,
+                                  env_name: "PLATFORM",
+                               description: "Plaform, ios or android",
+                                  optional: "ios",
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :tabs,
+                                  env_name: "TABS",
+                               description: "Array of all Google Sheet Tabs",
+                                  optional: false,
+                                      type: Array)
         ]
       end
 
